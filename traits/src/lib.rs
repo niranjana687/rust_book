2dcddc7eb2ef65690c4792d13b7c545a2f49f299
niranjana687 <niranjana687@gmail.com>
2020-07-16 19:28:00 +0530

    return types that implement traits(switching)

--- traits/src/lib.rs
+++ traits/src/lib.rs
@@ -59,37 +59,64 @@ pub fn notify<T: Summary + Display> (item: &T) {
 
 }
 
-fn some_function<T , U>(t :&T, u :&U) -> i32
-            where T: Display + Clone,
-                  U: Clone + Debug
-    {
+// fn some_function<T , U>(t :&T, u :&U) -> i32
+//             where T: Display + Clone,
+//                   U: Clone + Debug
+//     {
 
-    } 
-fn returns_summarizable() -> impl Summary {
-    Tweet{
-        username: String::from("Niranjana"),
-        content: String::from("Hello hi i feel so happy"),
-        reply: false,
-        retweet: false,
+//     }  
+// fn returns_summarizable() -> impl Summary {
+//     Tweet{
+//         username: String::from("Niranjana"),
+//         content: String::from("Hello hi i feel so happy",),
+//         reply: false,
+//         retweet: false,
 
+//     }
+// }
+pub struct NewsArticle {
+    pub headline: String,
+    pub author: String,
+    pub location: String,
+    pub content: String,
+}
+
+impl Summary for NewsArticle {
+    fn summarize_author(&self) -> String {
+        format!("{}", self.author)
+    }
+    fn summarize(&self) -> String {
+        format!("{}, by {}, {}", self.headline, self.author, self.location)
     }
 }
 
-// pub struct NewsArticle {
-//     pub headline: String,
-//     pub author: String,
-//     pub location: String,
-//     pub content: String,
-// }
+fn returns_summarizable(switch :bool) -> impl Summary {
+    if switch {
+        NewsArticle {
+            headline: String::from(
+                "Penguins win the Stanley Cup Championship!",
+            ),
+            location: String::from("Pittsburgh, PA, USA"),
+            author: String::from("Iceburgh"),
+            content: String::from(
+                "The Pittsburgh Penguins once again are the best \
+                 hockey team in the NHL."
+            ),
+        }
+    }
+    else {
+        Tweet {
+            username: String::from("horse_ebooks"),
+            content: String::from(
+                "of course, as you probably already know, people"
+            ),
+            reply: false,
+            retweet: false,
+        }
+    }
+}
 
-// impl Summary for NewsArticle {
-//     fn summarize_author(&self) -> String {
-//         format!("{}", self.author)
-//     }
-//     fn summarize(&self) -> String {
-//         format!("{}, by {}, {}", self.headline, self.author, self.location)
-//     }
-// }
+
 
 
 
