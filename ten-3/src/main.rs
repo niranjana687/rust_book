406a22fc2529727acc6cfc060ba824fbee1977e8
niranjana687 <niranjana687@gmail.com>
2020-07-18 12:34:36 +0530

    lifetime annotations in fn sign

--- ten-3/src/main.rs
+++ ten-3/src/main.rs
@@ -1,8 +1,23 @@
 fn main() {
     let r;
     
-        let x = 5;
-        r = &x;
+        let z = 5;
+        r = &z;
     
     println!("{}", r);
+
+    let s1 = String::from("abcde");
+    let s2 = "xyz";
+
+    let result = longest(s1.as_str(), s2);
+    println!("{}", result);
+}
+
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+    if x.len() > y.len(){
+        x
+    }
+    else {
+        y
+    }
 }
