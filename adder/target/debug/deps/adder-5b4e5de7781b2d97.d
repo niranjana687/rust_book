40a48c553d5da8b10354d39e5b848983ff5092ba
niranjana687 <niranjana687@gmail.com>
2020-07-19 10:05:30 +0530

    test definition

---
+++ adder/target/debug/deps/adder-5b4e5de7781b2d97.d
@@ -0,0 +1,7 @@
+/Users/niranjana/rust_book/adder/target/debug/deps/adder-5b4e5de7781b2d97.rmeta: src/lib.rs
+
+/Users/niranjana/rust_book/adder/target/debug/deps/libadder-5b4e5de7781b2d97.rlib: src/lib.rs
+
+/Users/niranjana/rust_book/adder/target/debug/deps/adder-5b4e5de7781b2d97.d: src/lib.rs
+
+src/lib.rs:
