6e4c05166f79c76fd350d7e1619cf7be65012e6f
niranjana687 <niranjana687@gmail.com>
2020-07-19 10:37:33 +0530

    first test

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -1,7 +1,38 @@
 #[cfg(test)]
 mod tests {
+    use super::*;
     #[test]
-    fn it_works() {
+    fn larger_can_hold_smaller() {
+        let larger = Rectangle {
+            width: 44,
+            height: 56,
+        };
+        let smaller = Rectangle {
+            width: 33,
+            height: 35,
+        };
+        assert!(larger.can_hold(&smaller));
+    }
+
+    #[test]
+    fn exploration() {
         assert_eq!(2 + 2, 4);
+    }
+
+    // #[test]
+    // fn this_fails() {
+    //     panic!("This test failed");
+    // }
+}
+
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn can_hold(&self, other :&Rectangle) -> bool {
+        self.width >= other.width && self.height >= other.height
     }
 }
