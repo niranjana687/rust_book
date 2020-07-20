3a9a7b6b9e2e22ac738bba4b5d607e0fcb55cfbd
niranjana687 <niranjana687@gmail.com>
2020-07-20 15:52:59 +0530

    integration tests

---
+++ adder/target/debug/deps/integration_test-d8af2d1e37280cad.d
@@ -0,0 +1,5 @@
+/Users/niranjana/rust_book/adder/target/debug/deps/integration_test-d8af2d1e37280cad: tests/integration_test.rs
+
+/Users/niranjana/rust_book/adder/target/debug/deps/integration_test-d8af2d1e37280cad.d: tests/integration_test.rs
+
+tests/integration_test.rs:
--- adder/target/debug/libadder.d
+++
@@ -1,1 +0,0 @@
-/Users/niranjana/rust_book/adder/target/debug/libadder.rlib: /Users/niranjana/rust_book/adder/src/lib.rs
