1f071d66799ddc30e1086eaa6bc09812d3f43aa6
niranjana687 <niranjana687@gmail.com>
2020-07-28 12:13:32 +0530

    closures completed

--- thirteen/src/main.rs
+++ thirteen/src/main.rs
@@ -1,56 +1,54 @@
-use std::thread;
-use std::time::Duration;
+struct Cacher<T>
+where
+    T: Fn(u32) -> u32,
+{
+    calculation: T,
+    value: Option<u32>,
+}
 
-// fn main() {
-//     let test = 4 ;
-//     if test > 3 {
-//   }
-  
-//   }
+impl<T> Cacher<T>
+where
+    T: Fn(u32) -> u32,
+{
+    fn new(calculation: T) -> Cacher<T> {
+        Cacher {
+            calculation,
+            value: None,
+        }
+    }
 
-fn generate_workout(intensity: u32, random_number: u32) {
-    let sec = |num: u32| -> u32 {
-        println!("Calculating slowly..");
-        thread::sleep(Duration::from_secs(2));
-        num
-    };
-    if intensity < 25 {
-        println!(
-            "Today, do {} pushups!", 
-            sec(intensity)
-        );
-        println!(
-            "Next, do {} situps", 
-            sec(intensity)    
-        );
-
-    }else {
-        if random_number == 3 {
-            println!("Take a break today!");
-        } else {
-            println!("Run for {} minutes!", sec(intensity));
+    fn value(&mut self, arg: u32) -> u32 {
+        match self.value {
+            Some(v) => v,
+            None => {
+                let v = (self.calculation)(arg);
+                self.value = Some(v);
+                v
+            }
         }
     }
 }
 
 fn main() {
-    let intensity  = 8;
-    let random_number = 5;
-   generate_workout(intensity, random_number);
-   fn add_v1(a : i32) -> i32 {
-       a + 1 
-   }
-   //closure type inference and annotation
-   let add_v2 = |a| {a + 1 };
-   let add_v3 = |a: i32| -> i32 { a + 1 };
-   let add_v4 =|a| a + 1;
-   let v1 = add_v1(1);
-   let v2 = add_v2(1);
-   let v3 = add_v3(1);
-   let v4 = add_v4(1);
-   println!("{} {} {} {} ", v1, v2, v3 , v4);
+    let x = 4;
+    let equal_to_x = |z| z==x;
+    let y = 4;
+    assert!(equal_to_x(y));
+
+
 }
 
+#[cfg(test)]
+mod tests {
+    use super::*;
 
+    #[test]
+    fn call_with_different_values() {
+        let mut c = Cacher::new(|a| a);
 
- 
+        let v1 = c.value(1);
+        let v2 = c.value(2);
+
+        assert_eq!(v2, 2);
+    }
+}
---
+++ thirteen/target/debug/deps/thirteen-0400b470fb8d8059.d
@@ -0,0 +1,5 @@
+/Users/niranjana/rust_book/thirteen/target/debug/deps/thirteen-0400b470fb8d8059: src/main.rs
+
+/Users/niranjana/rust_book/thirteen/target/debug/deps/thirteen-0400b470fb8d8059.d: src/main.rs
+
+src/main.rs:
