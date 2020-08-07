24194582b41fb68710c13f644c7585becbd38259
niranjana687 <niranjana687@gmail.com>
2020-08-07 23:59:20 +0530

    deref trait

--- box-on-heap/src/main.rs
+++ box-on-heap/src/main.rs
@@ -1,4 +1,19 @@
 use crate::List::{Cons, Nil};
+use std::ops::Deref;
+
+struct MyBox<T>(T);
+
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    } 
+}
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+    fn deref(&self) -> &T {
+        &self.0
+    }
+}
 #[derive(Debug)]
 enum List {
     Cons(i32, Box<List>),
@@ -11,4 +26,16 @@ fn main() {
 
     let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3,Box::new(Nil))))));
     println!("{:?}", list);
+
+    let x = 5;
+    let y = MyBox::new(x);
+    assert_eq!(5,x);
+    assert_eq!(5,*y );
+
+    let name = MyBox::new(String::from("Niranjana"));
+    hello(&name);
+}
+
+fn hello(name: &str)  {
+    println!("Hello, {}", name);
 }
