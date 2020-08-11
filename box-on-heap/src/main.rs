a94dfbc8d90cf030f3d63388148c8aec008bc6f5
niranjana687 <niranjana687@gmail.com>
2020-08-11 14:52:32 +0530

    ref counter complete

--- box-on-heap/src/main.rs
+++ box-on-heap/src/main.rs
@@ -1,6 +1,7 @@
 use crate::List::{Cons, Nil};
 use std::ops::Deref;
 use std::mem::drop;
+use std::rc::Rc;
 
 struct MyBox<T>(T);
 
@@ -28,21 +29,21 @@ impl Drop for CustomSmartPointer {
 
 #[derive(Debug)]
 enum List {
-    Cons(i32, Box<List>),
+    Cons(i32, Rc<List>),
     Nil,
 }
 
 fn main() {
-    let b = Box::new(5);
-    println!("{}", b);
+    // let b = Box::new(5);
+    // println!("{}", b);
 
-    let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3,Box::new(Nil))))));
-    println!("{:?}", list);
+    // let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3,Box::new(Nil))))));
+    // println!("{:?}", list);
 
-    let x = 5;
-    let y = MyBox::new(x);
-    assert_eq!(5,x);
-    assert_eq!(5,*y );
+    // let x = 5;
+    // let y = MyBox::new(x);
+    // assert_eq!(5,x);
+    // assert_eq!(5,*y );
 
     let name = MyBox::new(String::from("Niranjana"));
     hello(&name);
@@ -53,7 +54,26 @@ fn main() {
     println!("Smart Pointer is created");
     drop(d);
 
-    println!("d dropped before the end of main")
+    println!("d dropped before the end of main");
+
+    let a = Rc::new(Cons(10, Rc::new(Cons(20, Rc::new(Cons(30, Rc::new(Nil)))))));
+    let b = Cons(1, Rc::clone(&a));
+    let c = Cons(2, Rc::clone(&a));
+
+    let d = Rc::new(Cons(1, Rc::new(Nil)));
+    println!("d{}", Rc::strong_count(&d));
+
+    let e = Cons(1, Rc::clone(&d));
+    println!("e{}", Rc::strong_count(&d));
+
+    {
+        let f = Cons(2, Rc::clone(&d));
+        println!("f{}", Rc::strong_count(&d));
+    
+    }
+    println!("out{}", Rc::strong_count(&d));
+
+
 }
 
 fn hello(name: &str)  {
