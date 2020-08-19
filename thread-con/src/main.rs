07d0056b0f6fd472876b48b9defa00e11905e528
niranjana687 <niranjana687@gmail.com>
2020-08-19 23:32:28 +0530

    spwaning

--- thread-con/src/main.rs
+++ thread-con/src/main.rs
@@ -1,3 +1,17 @@
+use std::thread;
+use std::time::Duration;
+
+
 fn main() {
-    println!("Hello, world!");
+    thread::spawn(|| {
+        for i in 1..10{
+            println!("number from spwaned thread {}", i);
+            thread::sleep(Duration::from_millis(1));
+        }
+    });
+
+    for i in 1..5 {
+        println!("number from main thread {}", i);
+        thread::sleep(Duration::from_millis(1));
+    }
 }
