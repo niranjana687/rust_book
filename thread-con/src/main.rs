901b0225c8f49c50866f328ace59c8dbfb019a73
niranjana687 <niranjana687@gmail.com>
2020-08-24 12:08:34 +0530

    move closure

--- thread-con/src/main.rs
+++ thread-con/src/main.rs
@@ -1,17 +1,28 @@
 use std::thread;
-use std::time::Duration;
+// use std::time::Duration;
 
 
 fn main() {
-    thread::spawn(|| {
-        for i in 1..10{
-            println!("number from spwaned thread {}", i);
-            thread::sleep(Duration::from_millis(1));
-        }
-    });
+    
 
-    for i in 1..5 {
-        println!("number from main thread {}", i);
-        thread::sleep(Duration::from_millis(1));
-    }
+    // let handle = thread::spawn(|| {
+    //     for i in 1..10{
+    //         println!("number from spwaned thread {}", i);
+    //         thread::sleep(Duration::from_millis(1));
+    //     }
+    // });
+    // handle.join().unwrap();
+
+    // for i in 1..5 {
+    //     println!("number from main thread {}", i);
+    //     thread::sleep(Duration::from_millis(1));
+    // }
+    let v = vec![1,2,3,4,5,6];
+    let handle = thread::spawn( move || {
+        println!("The vector is {:?}",v);
+        
+    });
+    // drop(v); //big oops!
+
+    handle.join().unwrap();
 }
